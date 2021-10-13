faab6600b2a010378299f1a2f5bb6bf59cd8c75f
mohak300501 <mohak_k@ph.iitr.ac.in>
2021-10-13 14:33:59 +0530

    integrate

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-const port = process.env.SERVER_PORT
+const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
 })
